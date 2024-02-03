edba356cf10b5e194d2badb0b266726434fcdac0
Marcio Curvello <marciocurvello@gmail.com>
2024-02-03 08:11:47 -0300

    Landmark file

--- Landmarks/Landmark.swift
+++ Landmarks/Landmark.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import SwiftUI
+import CoreLocation
 
 struct Landmark: Hashable, Codable {
     var id: Int
@@ -13,4 +15,19 @@ struct Landmark: Hashable, Codable {
     var park: String
     var state: String
     var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: coordinates.latitute, longitude: coordinates.longitude)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitute: Double
+        var longitude: Double
+    }
 }
