c183b987df7e5ec3ea5c618006d0b8811947a013
Marcio Curvello <marciocurvello@gmail.com>
2024-02-11 12:01:12 -0300

    add PageView

--- Landmarks/Views/PageView/PageView.swift
+++ Landmarks/Views/PageView/PageView.swift
@@ -7,12 +7,15 @@
 
 import SwiftUI
 
-struct PageView: View {
+struct PageView<Page: View>: View {
+    var pages: [Page]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        PageViewController(pages: pages)
+            .aspectRatio(3 / 2, contentMode: .fit)
     }
 }
 
 #Preview {
-    PageView()
+    PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
 }
--- Landmarks/Views/PageView/PageViewController.swift
+++ Landmarks/Views/PageView/PageViewController.swift
@@ -5,4 +5,21 @@
 //  Created by Marcio Curvello on 11/02/24.
 //
 
-import Foundation
+import SwiftUI
+import UIKit
+
+struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    var pages: [Page]
+    
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal)
+        
+        return pageViewController
+    }
+    
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers([UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+    }
+}
